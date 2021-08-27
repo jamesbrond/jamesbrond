ca2ab536932814813d1de091a371fe986e1f411c
jbrond <diego.brondo@philips.com>
2021-08-27 16:17:05 +0200

    add stuff

---
+++ jarfinder.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import logging
+import subprocess
+import sys
+
+def main():
+  parser = argparse.ArgumentParser(description='Searches for the class in all JARs in specified folder.')
+  parser.add_argument('-p', '--path', required=True, help="path to the JAR files folder")
+  parser.add_argument('-n', '--name', required=True, help="name of the class (or part of it)")
+  parser.add_argument('-q', '--quiet', action='store_const', const=logging.NOTSET, dest='logLevel', help="suppress debug messages")
+  parser.add_argument('-v', '--verbose', action='store_const', const=logging.WARNING, dest='logLevel', help="increase verbosity level")
+  parser.add_argument('-vv', '--very-verbose', action='store_const', const=logging.DEBUG, dest='logLevel', help="increase verbosity to higher level")
+  parser.add_argument('--version', action='version', version='%(prog)s 2.0')
+  args = parser.parse_args()
+
+  logLevel = logging.ERROR if args.logLevel == None else args.logLevel
+  logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logLevel)
+  logger = logging.getLogger('JARFinder')
+
+  logger.debug(args)  
+
+  files = os.listdir(args.path)
+  for f in files:
+    if f.endswith(".jar"):
+      logger.debug('found jar file %s', (args.path+'/'+f))
+
+      proc = subprocess.run(
+        'jar tf ' + args.path+ '/' + f, 
+        shell=True, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.STDOUT
+      )
+      output = proc.stdout.decode('utf-8')
+      
+      if proc.returncode != 0:
+        logger.error(output)
+        sys.exit()
+
+      if args.name in output:
+        print('match found in ' + f)
+
+if __name__ == "__main__":
+  main()
---
+++ jdhack.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import errno
+import os
+import subprocess
+import sys
+
+def _mkdir(newdir):
+  try:
+    os.makedirs(newdir)
+  except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(newdir):
+      pass
+    else:
+      raise
+
+parser = argparse.ArgumentParser(description='Java Decompiler Hack')
+parser.add_argument('-c', '--classpath', metavar='CLASSPATH', required=True, help='Path where class to be decompiled are')
+parser.add_argument('-jd', '--jdjarpath', metavar='JD', required=True, help='Path to jd-cli.jar, if not defines jd-cli.jar must be in Java Library Path. https://github.com/kwart/jd-cmd')
+parser.add_argument('-s', '--srcpath', metavar='SRCPATH', required=False, help='Path where decompiled java will be placed (default src)')
+
+args = parser.parse_args()
+classpath = args.classpath
+jd = args.jdjarpath
+src = args.srcpath
+
+if not os.path.exists(classpath) or not  os.path.isdir(classpath):
+  print('CLASSPATH does not exist or is not a valid folder')
+  sys.exit(192)
+
+if not src:
+  src = 'src'
+
+_mkdir(src)
+
+with open('jd.log', 'wb') as jd_cmd_out:
+  for root, subdirs, files in os.walk(classpath):
+    print(32 * '-', '\nFolder: ', root)
+    for filename in files:
+      if filename.endswith('.class'):
+        print('\t', filename)
+        subprocess.call(['java.exe', '-jar', jd, os.path.join(root, filename), '--outputDir', os.path.join(src, root.replace(classpath, '').lstrip(os.sep))], stdout=jd_cmd_out, stderr=jd_cmd_out)
+
+print(32 * '-', 'done\ncheck "jd.log" file for jd-cli output');
+
+# ~@:-]
